ac03dae776fe32df4b259030c3121675c32c5d15
Mitch Polifka <mjpolifka@gmail.com>
2026-04-20 13:17:27 -0500

    removed unneeded process, tested

--- move-window.go
+++ move-window.go
@@ -10,7 +10,6 @@ import (
 func moveWindow(windowName string) error {
 	user32Dll := syscall.NewLazyDLL("user32.dll")
 	moveWindowProc := user32Dll.NewProc("MoveWindow")
-	showWindowProc := user32Dll.NewProc("ShowWindow")
 	setForegroundWindowProc := user32Dll.NewProc("SetForegroundWindow")
 	bringWindowToTopProc := user32Dll.NewProc("BringWindowToTop")
 
@@ -37,8 +36,6 @@ func moveWindow(windowName string) error {
 		return err
 	}
 
-	const swRestore = 9
-	showWindowProc.Call(hwnd, uintptr(swRestore))
 	bringWindowToTopProc.Call(hwnd)
 	setForegroundWindowProc.Call(hwnd)
 
