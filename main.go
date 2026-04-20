90c0f5af7c88aa132a9c7d617d2d4dcbf7269256
Mitch Polifka <mjpolifka@gmail.com>
2026-04-20 11:39:08 -0500

    renamed function

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	user32()
+	moveWindow()
 }
--- move-window.go
+++ move-window.go
@@ -5,9 +5,9 @@ import (
 	"syscall"
 )
 
-func user32() {
-	user32 := syscall.NewLazyDLL("user32.dll") // is this a real DLL?
-	moveWindow := user32.NewProc("MoveWindow")
+func moveWindow() {
+	user32Dll := syscall.NewLazyDLL("user32.dll")
+	moveWindowProc := user32Dll.NewProc("MoveWindow")
 
 	// Replace this with the HWND you want to resize.
 	var hwnd uintptr = 0x00123456 // what's an HWND and how do I get one?
@@ -17,9 +17,8 @@ func user32() {
 	width := 800
 	height := 600
 	repaint := 1 // TRUE
-	// Do we need to choose a display?
 
-	ret, _, err := moveWindow.Call(
+	ret, _, err := moveWindowProc.Call(
 		hwnd,
 		uintptr(x),
 		uintptr(y),
