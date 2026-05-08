8fc190300f1a15dd6ccd83d0da38405f554e7278
Mitch Polifka <mjpolifka@gmail.com>
2026-05-08 14:27:12 -0500

    passing TestValidatePlaceFile

--- fileIO_test.go
+++ fileIO_test.go
@@ -1,15 +1,80 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
 
 func TestValidatePlaceFile(t *testing.T) {
 	// test file doesn't exist
+	t.Run("file-doesnt-exist", func(t *testing.T) {
+		// change to T.TempDir, then file won't exist
+		t.Chdir(t.TempDir())
+
+		// start test
+		exist, _, _, err := validatePlaceFile()
+		if err != nil {
+			t.Fatal("want: exist==false | got: ERROR")
+		}
+		if exist {
+			t.Error("want: exist==false | got: exist==true")
+		}
+	})
 	// test file exists but is invalid
+	t.Run("file-is-corrupt", func(t *testing.T) {
+		// setup test
+		tempDir := t.TempDir()
+		t.Chdir(tempDir)
+		path := filepath.Join(tempDir, "place.json")
+		placeFile := `{"selected_location": "desktop","locations": [{"name": "laptop","places": ["bad"]},{"name": "desktop","places": []}]}`
+
+		if err := os.WriteFile(path, []byte(placeFile), 0644); err != nil {
+			t.Fatal("Couldn't set up test:", err)
+		}
+
+		// start test
+		exist, valid, _, err := validatePlaceFile()
+		if err != nil {
+			t.Fatal("want: exist==true && valid==false | got:", err)
+		}
+		if !exist {
+			t.Error("want: exist==true && valid==false | got: exist==false")
+		}
+		if valid {
+			t.Error("want: exist==true && valid==false | got: valid==true")
+		}
+	})
 	// test file exists and is valid
-	t.Error("Not yet implemented")
+	t.Run("file-is-valid", func(t *testing.T) {
+		// setup test
+		tempDir := t.TempDir()
+		t.Chdir(tempDir)
+		path := filepath.Join(tempDir, "place.json")
+		placeFile := PlaceFile{SelectedLocation: "desktop", Locations: []Location{}}
+		placeFile.Locations = append(placeFile.Locations, Location{Name: "desktop", Places: []Place{}})
+		jsonBytes, err := json.Marshal(placeFile)
+		if err != nil {
+			t.Fatal("Couldn't set up test:", err)
+		}
+		if err = os.WriteFile(path, jsonBytes, 0644); err != nil {
+			t.Fatal("Couldn't set up test:", err)
+		}
+
+		// start test
+		exist, valid, _, err := validatePlaceFile()
+		if err != nil {
+			t.Fatal("want: exist==true && valid==true | got:", err)
+		}
+		if !exist {
+			t.Error("want: exist==true && valid==true | got: exist==false")
+		}
+		if !valid {
+			t.Error("want: exist==true && valid==true | got: valid==false")
+		}
+	})
 }
 
 func TestGetUserInput(t *testing.T) {
@@ -17,7 +82,7 @@ func TestGetUserInput(t *testing.T) {
 	in := strings.NewReader("test string\n")
 	got, err := getUserInput(in)
 	if err != nil {
-		t.Error("want: no error | got:", err)
+		t.Fatal("want: no error | got:", err)
 	}
 	if got != "test string" {
 		t.Error("want: test string | got:", got)
